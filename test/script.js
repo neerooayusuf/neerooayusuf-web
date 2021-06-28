443176f339cad2d2fde26b5d56a8c7a448cc370f
myusuf05 <ymnpsc@gmail.com>
2021-06-28 11:13:31 +0400

    added test branch

---
+++ test/script.js
@@ -0,0 +1,18 @@
+function functionOne() {
+	window.location.href = "./myusuf05";
+}
+
+function functionTwo() {
+	window.location.href = "https://realms.gg/WuY-LoRreCo";
+}
+
+function functionThree() {
+	window.location.href = "./twitch";
+}
+
+function functionFour() {
+	window.location.href = "./f1";
+}
+function functionFive() {
+	window.location.href = "./notepad";
+}
